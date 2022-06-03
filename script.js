0c3b373d29a05b801f8c6d4449dc6ea27d046022
Aashutosh Bansal <75746087+coderAashutosh@users.noreply.github.com>
2022-06-03 19:38:19 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ copyBtn.addEventListener('click', () => {
     Toastify({
         text: "Quote Copied!",
         duration: 2000,
-        destination: "https://aashutosh.vercel.app",
+        destination: "https://aashutosh.vercel.app/",
         newWindow: true,
         // close: true,
         gravity: "top", // `top` or `bottom`
